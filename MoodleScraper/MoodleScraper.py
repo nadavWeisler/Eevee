7c36757727e9a831bb8b7d877a745e3d9f995979
Ben Gofrit <t-bengofrit@microsoft.com>
2022-04-07 15:53:04 +0300

    automatic token generation

--- MoodleScraper/MoodleScraper.py
+++ MoodleScraper/MoodleScraper.py
@@ -1,5 +1,39 @@
 import moodle_api
+import KeyGenerator
 
-# courses = moodle_api.CourseList()
-course5 = moodle_api.call('core_course_get_contents', courseid=52307)
-course5
+class MoodleConnector:
+    def __init__(self):
+        with open("credentials", "r") as cred:
+            self._username, self._pass = cred.readline().split(',')
+
+    def get_course_contents_wrapper(self, courseid):
+        token = KeyGenerator.get_token_from_credentials(username=self._username, password=self._pass)
+        moodle_api.KEY = token
+        return moodle_api.call('core_course_get_contents', courseid=courseid)
+
+
+if __name__ == '__main__':
+    moodleConnector = MoodleConnector()
+    course1 = moodleConnector.get_course_contents_wrapper(52307)
+    course2 = moodleConnector.get_course_contents_wrapper(52311)
+    course3 = moodleConnector.get_course_contents_wrapper(67506)
+    course1 = moodleConnector.get_course_contents_wrapper(52307)
+    course1 = moodleConnector.get_course_contents_wrapper(52307)
+    course1 = moodleConnector.get_course_contents_wrapper(52307)
+    course1 = moodleConnector.get_course_contents_wrapper(52307)
+
+
+
+
+
+
+#
+#     course6 = moodle_api.call('core_course_get_contents', courseid=52311)
+#     course7 = moodle_api.call('core_course_get_contents', courseid=67506)
+#     course = moodle_api.call('core_course_get_contents', courseid=52307)
+#     print(token)
+# # courses = moodle_api.CourseList()
+# print(courses)
+
+
+# course5
