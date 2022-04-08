6db583a0dfa1979f64ea654e6f96d2fa40faca41
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2022-04-08 04:01:34 +0300

    for deploy

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import __data__ as data
 
 app = Flask(data.__app_name__)
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -23,15 +24,16 @@ def index():
         r = requests.post(os.environ.get("SIGNUP_HOST")+"?name=" + name + '&email=' + email + '&trello_api_key=' +
                           trello1 + '&trello_api_secret=' + trello2 + '&trello_token=' + trello3 + '&moodle_username=' + moodleUserName + '&moodle_password=' + moodlePassword)
 
-        print(r.text)
         result = r.text.find('"rt": "1"') != -1
         if result:
             return send_from_directory('templates', 'thankYou.html')
         else:
             return send_from_directory('templates', 'error.html')
-        
 
 
 @app.route('/favicon.ico')
 def icon():
     return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+app.run(port=4500)
