a4112659002a9c58438afcc898effc203fa6e96a
Ben Gofrit <t-bengofrit@microsoft.com>
2022-04-08 05:26:57 +0300

    Wrapper for Trello API

--- TrelloConnector/TrelloConnector.py
+++ TrelloConnector/TrelloConnector.py
@@ -1,11 +1,13 @@
-from MoodleScraper import Tasks
+# from MoodleScraper import Tasks
 from trello import TrelloClient
-
+# from MoodleScraper import lambda_function
+from datetime import datetime
 
 class TrelloConnector:
     def _get_or_create_board(self):
         all_boards = self.client.list_boards()
         eevee_board = None
+        self.type_to_color = {'assign': 'red', 'quiz': 'blue'}
 
         for board in all_boards:
             if board.name == "Eevee":
@@ -51,7 +53,7 @@ class TrelloConnector:
         :param task_name:
         :return: true if taskname is in the board
         """
-        return task_name in [card.name for card in self.eevee_board.all_cards()]
+        return task_name in {card.name for card in self.eevee_board.all_cards()}
 
 
     def post_courses_tasks(self, payload):
@@ -67,15 +69,23 @@ class TrelloConnector:
                 task_name = task.name
                 if not self.is_task_in_board(task_name):
                     updated = True
-                    trello_course_list.add_card(task_name)
+                    due_date = datetime.fromtimestamp(task.due_date).strftime("%d %b, %Y") if task.due_date else 'null'
+                    # label = [self.type_to_color[task.type] if task.type else None]
+                    desc = task.url if task.url else None
+                    trello_course_list.add_card(name=task_name, due=due_date, desc=desc)
         return updated
 
-if __name__ == '__main__':
-    connector = TrelloConnector("c32bd037befedb2ba4d1563d0c98c9fa","40f8b539dc29ebd8c02b1712238a23a6e5c5066b09bea95976a384e3a435cfb6","b4a7f2483e82c020f262abcaf4e710c4ddf147ea66377bec28f8bf6011dbdcf6")
-    g = connector.get_or_create_course_list(123)
-    g2 = connector.get_or_create_course_list(123)
-    g2.add_card("ex21")
-    print(connector.is_task_in_board("ex21"))
-    print(connector.is_task_in_board("ex22"))
+# if __name__ == '__main__':
+    # test_dict = lambda_function.zibi()
+    # short_dict = {list(test_dict.keys())[0] : test_dict[list(test_dict.keys()[0])]}
+    #
+    # print(test_dict)
+    # connector = TrelloConnector("c32bd037befedb2ba4d1563d0c98c9fa","40f8b539dc29ebd8c02b1712238a23a6e5c5066b09bea95976a384e3a435cfb6","b4a7f2483e82c020f262abcaf4e710c4ddf147ea66377bec28f8bf6011dbdcf6")
+    # connector.post_courses_tasks(test_dict)
+    # g = connector.get_or_create_course_list(123)
+    # g2 = connector.get_or_create_course_list(123)
+    # g2.add_card("ex21")
+    # print(connector.is_task_in_board("ex21"))
+    # print(connector.is_task_in_board("ex22"))
     # print(g2.name)
     # print(connector.get_board().name)
